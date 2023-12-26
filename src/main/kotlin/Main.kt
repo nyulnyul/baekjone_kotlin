4fcedc2d2ef3ced771265da5439e4471fa266fe0
nyulnyul <nyul0223@naver.com>
2023-12-26 10:56:18 +0900

    2739 구구단

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,9 @@
 import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
-    var a =nextInt()
-    var b = nextInt()
-    val c = nextInt()
+    var n =nextInt()
 
-    if(a==b && b==c) {
-        println(10000 + a * 1000)
-    } else if(a==b || a==c) {
-        println(1000 + a * 100)
-    } else if(b==c) {
-        println(1000 + b * 100)
-    } else {
-        println(Math.max(Math.max(a, b), c) * 100)
+    for(i in 1..9){
+        println("$n * $i = ${n*i}")
     }
 }
