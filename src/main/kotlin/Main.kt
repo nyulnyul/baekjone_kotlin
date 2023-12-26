66a1b0f3208d23d3db22e31c3271c3d4e4685e8d
nyulnyul <nyul0223@naver.com>
2023-12-26 11:02:34 +0900

    10950번 a+b-3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,12 @@
-import java.util.*
+import java.util.Scanner
 
 fun main() = with(Scanner(System.`in`)) {
-    var n =nextInt()
+    val num = nextInt()
 
-    for(i in 1..9){
-        println("$n * $i = ${n*i}")
+    for (i in 1..num) {
+        var A = nextInt()
+        var B = nextInt()
+
+        println("${A + B}")
     }
 }
