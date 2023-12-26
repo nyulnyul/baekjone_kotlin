3337284b4f4e237cb0a0f6a157912603e27af2d8
nyulnyul <nyul0223@naver.com>
2023-12-26 11:43:09 +0900

    25314 코딩은 체육과목 입니다

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,18 +2,11 @@ import java.util.Scanner
 
 fun main() = with(Scanner(System.`in`)) {
     val x = nextInt()
-
-    var n = nextInt()
-    var ab =0
-
-    for(i in 1..n){
-        var a = nextInt()
-        var b = nextInt()
-        ab += a*b
+    val longcnt = x/4
+    for(i in 1..longcnt){
+        print("long ")
     }
-
-    if(x == ab) println("Yes")
-    else println("No")
+    print("int")
 
 
 }
