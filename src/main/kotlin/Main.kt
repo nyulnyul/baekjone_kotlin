b2816d142e33a5b25ffa8d728c7dfdb402b204ae
nyulnyul <nyul0223@naver.com>
2023-12-27 11:00:36 +0900

    10817 세 수

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,23 @@
 import java.time.LocalDateTime
 import java.time.LocalTime
+import java.util.ArrayList
 import java.util.Scanner
+import kotlin.math.max
+
 fun main() = with(Scanner(System.`in`)) {
-   val x = nextInt()
-    val y = nextInt()
-    val time = LocalDateTime.of(2007,x, y,0,0)
+    val a = nextInt()
+    val b = nextInt()
+    val c = nextInt()
 
-    val day = time.dayOfWeek.toString()
-    println(day.substring(0,3))
+    val list : ArrayList<Int> = arrayListOf(a,b,c)
+    list.sort()
+    print(list[1])
 
-}
+    }
+
+
+
+
+
+
+
