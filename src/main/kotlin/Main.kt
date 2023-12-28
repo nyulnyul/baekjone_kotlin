3a9550226b020913a0d0c961f4f7b6880444fff2
nyulnyul <nyul0223@naver.com>
2023-12-28 14:56:45 +0900

    1929 소수 구하기

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,22 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.Scanner
+import kotlin.math.sqrt
+
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    var n = readLine().toInt()
-    var a = readLine().split(" ").filter { it.isNotEmpty() }.map { it.toInt() }.sorted()
 
+   while(true){
+       val n = readLine() ?: break
+   }
 
-        println(a[0] * a.last())
+}
 
+fun getOne(n: Int): Int {
+    var result = 0
+    for (i in 1..n) {
+        if (i % 10 == 1) {
+            result++
+        }
+    }
+    return result
 }
