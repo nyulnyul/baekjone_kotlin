30f4c31e65e94211d66ab1fc6598c05a5750e4d9
nyulnyul <nyul0223@naver.com>
2023-12-29 14:39:41 +0900

    4101  크냐?

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,13 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-
-    while (true){
-        var a = readLine()
-        if(a == "0") break
-        println(if (a == a.reversed()) "yes" else "no")
+    while (true) {
+        val (a, b) = readLine().split(" ").map { it.toInt() }
+        if (a == 0 && b == 0) break
+        if (a > b) {
+            println("Yes")
+        } else {
+            println("No")
+        }
     }
 }
