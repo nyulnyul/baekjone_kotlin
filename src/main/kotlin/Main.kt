07785442a7abd15135321907f85dca9c3f82096a
nyulnyul <nyul0223@naver.com>
2023-12-29 15:06:53 +0900

    4892번 숫자 맞추기 게임

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,20 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    var cnt = 1
     while (true) {
-        val (a, b) = readLine().split(" ").map { it.toInt() }
-        if (a == 0 && b == 0) break
-        if (a > b) {
-            println("Yes")
+        val a = readLine().toInt()
+        if (a == 0) break
+
+        print("$cnt. ")
+        val b = 3 * a
+        if (b % 2 == 0) {
+            val e = (b / 2) * 3 / 9
+            println("even $e")
         } else {
-            println("No")
+            val e = ((b + 1) / 2) * 3 / 9
+            println("odd $e")
         }
+        cnt++
     }
 }
