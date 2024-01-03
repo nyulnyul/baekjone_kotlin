8833c6d4d15ab9fcf2cce1e302b90e9aa83dec17
nyulnyul <nyul0223@naver.com>
2024-01-03 18:16:03 +0900

    10872 팩토리얼

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,19 +3,9 @@ import java.io.InputStreamReader
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     var n = readLine().toInt() ?: return
-    var list = mutableListOf<Int>()
-    for(i in 0..n-1){
-        list.add(readLine().toInt())
-
-
+    var sum = 1
+    for(i in 1..n) {
+        sum *= i
     }
-    if(list[1]-list[0] == list[2]-list[1]){
-        println(list.last() + list[1]-list[0])}
-    else {
-        println(list.last() * (list[1]/list[0]))
-    }
-
-
-
-
+    println(sum)
 }
