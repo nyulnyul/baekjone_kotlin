65ef28c4880fa86dab1e7e80e1128235bb43b133
nyulnyul <nyul0223@naver.com>
2024-01-03 18:13:22 +0900

    1731 추론

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,25 +2,20 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    var n = readLine().toInt() ?: return
+    var list = mutableListOf<Int>()
+    for(i in 0..n-1){
+        list.add(readLine().toInt())
 
-    var n = readLine() ?: return
-    var smileCnt =0
-    var sadCnt =0
-    for(i in 0..n.length-3){
-
-       when{
-           n.startsWith(":-)",i) -> smileCnt++
-              n.startsWith(":-(",i) -> sadCnt++
-
-       }
 
     }
-   when{
-       smileCnt == 0 && sadCnt == 0 -> println("none")
-       smileCnt == sadCnt -> println("unsure")
-       smileCnt > sadCnt -> println("happy")
-       smileCnt < sadCnt -> println("sad")
-   }
+    if(list[1]-list[0] == list[2]-list[1]){
+        println(list.last() + list[1]-list[0])}
+    else {
+        println(list.last() * (list[1]/list[0]))
+    }
+
+
 
 
 }
