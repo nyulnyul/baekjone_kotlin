799a11dc1de6976f7886e158a9e95934b9383c82
nyulnyul <nyul0223@naver.com>
2024-01-03 15:13:56 +0900

    1264 모음 개수

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,20 +2,13 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    var cnt = 1
-    while (true) {
-        val a = readLine().toInt()
-        if (a == 0) break
+    val regex = "[aeiouAEIOU]".toRegex()
 
-        print("$cnt. ")
-        val b = 3 * a
-        if (b % 2 == 0) {
-            val e = (b / 2) * 3 / 9
-            println("even $e")
-        } else {
-            val e = ((b + 1) / 2) * 3 / 9
-            println("odd $e")
-        }
-        cnt++
+    while (true) {
+        val n = readLine() ?: break
+        if (n == "#") break // '#' 입력 시 루프 종료
+
+        val cnt = n.count { it.toString().matches(regex) } // 모음 개수 세기
+        println(cnt)
     }
 }
