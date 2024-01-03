920e6049fd486c3723937421596afb598e7806e3
nyulnyul <nyul0223@naver.com>
2024-01-03 18:18:46 +0900

    10987 모음의 개수

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,8 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    var n = readLine().toInt() ?: return
-    var sum = 1
-    for(i in 1..n) {
-        sum *= i
-    }
-    println(sum)
+    var n = readLine() ?: return
+    var regex = Regex("[a.e.i.o.u]")
+    n.chunked(1).count() { it.matches(regex) }.let { println(it) }
+
 }
