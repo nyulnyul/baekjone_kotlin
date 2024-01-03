a85ebd2085d3c73635c65885cefb220aae26bf46
nyulnyul <nyul0223@naver.com>
2024-01-03 17:54:24 +0900

    10769 행복한지 슬픈지

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,25 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    val regex = "[aeiouAEIOU]".toRegex()
 
-    while (true) {
-        val n = readLine() ?: break
-        if (n == "#") break // '#' 입력 시 루프 종료
+    var n = readLine() ?: return
+    var smileCnt =0
+    var sadCnt =0
+    for(i in 0..n.length-3){
 
-        val cnt = n.count { it.toString().matches(regex) } // 모음 개수 세기
-        println(cnt)
+       when{
+           n.startsWith(":-)",i) -> smileCnt++
+              n.startsWith(":-(",i) -> sadCnt++
+
+       }
+
     }
+   when{
+       smileCnt == 0 && sadCnt == 0 -> println("none")
+       smileCnt == sadCnt -> println("unsure")
+       smileCnt > sadCnt -> println("happy")
+       smileCnt < sadCnt -> println("sad")
+   }
+
+
 }
