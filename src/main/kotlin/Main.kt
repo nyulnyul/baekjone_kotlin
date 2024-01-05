a6d88788658c0b3122a9f461f920ca2d1cfb3efe
nyulnyul <nyul0223@naver.com>
2024-01-05 14:52:32 +0900

    11586 지영 공주님의 마법 거울

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,32 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    var n = readLine() ?: return
-    var regex = Regex("[a.e.i.o.u]")
-    n.chunked(1).count() { it.matches(regex) }.let { println(it) }
+    var n = readLine().toInt()
+    var a = Array(n){""}
+    for(i in 0 until n) {
+        a[i] = readLine()
+
+    }
+    var res = readLine().toInt()
+    when(res){
+        1 ->{
+            for(i in a){
+                println(i)
+            }
+        }
+        2 ->{
+            for(i in a){
+                println(i.reversed())
+            }
+        }
+        3->{
+            for(i in n -1 downTo 0){
+                println(a[i])
+            }
+        }
+    }
+
+
+
 
 }
