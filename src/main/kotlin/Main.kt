5bbacbf8b5dcdb763d4ac71f57bdc11ef3c7e3e5
nyulnyul <nyul0223@naver.com>
2024-01-06 19:11:04 +0900

    4458 첫글자를 대문자로

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,29 +5,11 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     var n = readLine().toInt()
     var a = Array(n){""}
     for(i in 0 until n) {
-        a[i] = readLine()
+        var s =readLine()
 
+        a[i] = s.replaceFirstChar { it.toUpperCase() }
+
+        println(a[i])
     }
-    var res = readLine().toInt()
-    when(res){
-        1 ->{
-            for(i in a){
-                println(i)
-            }
-        }
-        2 ->{
-            for(i in a){
-                println(i.reversed())
-            }
-        }
-        3->{
-            for(i in n -1 downTo 0){
-                println(a[i])
-            }
-        }
-    }
-
-
-
 
 }
